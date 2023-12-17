b781afffcae99dc2913a3f7e1cacc5607f9f11d7
Phuriphat Wongkhachonloetmetha <6510110374@email.psu.ac.th>
2023-12-17 22:03:31 +0700

    test pylint

--- testpytest.py
+++ testpytest.py
@@ -1,6 +1,15 @@
-# content of test_sample.py
+"""
+Module docstring: This module contains functions for testing purposes.
+"""
+
 def func(x):
+    """
+    This function adds 1 to the input.
+    """
     return x + 1
 
 def test_answer():
+    """
+    This function tests the func function.
+    """
     assert func(3) == 4
